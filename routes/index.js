64d4eb011cee6ef1f0d1d11fb8700545d0e6b418
Olga Bobrova <eve982@yandex.ru>
2023-07-25 14:22:01 +0300

    swagger doesnt work yet

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('../middlewares/swagger');
 const usersRoutes = require('./users');
 const moviesRoutes = require('./movies');
 const auth = require('../middlewares/auth');
@@ -6,6 +8,7 @@ const NotFoundError = require('../errors/not_found_error');
 const { createUser, login, logout } = require('../controllers/users');
 const { validateLogin, validateCreateUser } = require('../middlewares/validatiors');
 
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 router.post('/signin', validateLogin, login);
 router.post('/signup', validateCreateUser, createUser);
 router.use(auth);
