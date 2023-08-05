0f9e065fdf41adfe74d2740f279b0e38ec7c6ec0
Olga Bobrova <eve982@yandex.ru>
2023-08-05 21:06:03 +0300

    delete some regex constants

--- utils/constants.js
+++ utils/constants.js
@@ -4,8 +4,8 @@ const UNAUTORIZED = 401;
 const SERVER_ERROR = 500;
 const REGEX_LINK = /^https?:\/\/(www.)?\S+\.\S+#?/i;
 // const REGEX_COUNTRY = /^[^"]+"([^"]+)"$/;
-const REGEX_RU_LANG = /^[а-яА-ЯёЁ\s]+$/;
-const REGEX_EN_LANG = /^[a-zA-Z\s\d\-()]+$/;
+// const REGEX_RU_LANG = /^[а-яА-ЯёЁ\s]+$/;
+// const REGEX_EN_LANG = /^[a-zA-Z\s\d\-()]+$/;
 const REGEX_YEAR = /^(19\d{2}|20\d{2})$/;
 const ALLOWED_CORS = [
   'https://eve982.movies.nomoredomains.xyz',
@@ -24,8 +24,8 @@ module.exports = {
   SERVER_ERROR,
   REGEX_LINK,
   // REGEX_COUNTRY,
-  REGEX_RU_LANG,
-  REGEX_EN_LANG,
+  // REGEX_RU_LANG,
+  // REGEX_EN_LANG,
   REGEX_YEAR,
   ALLOWED_CORS,
   LOGIN_MESSAGE,
